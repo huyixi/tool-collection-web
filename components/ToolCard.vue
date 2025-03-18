43db22ae38ead8b5980e53d8c66801b9a8cfdbd7
huyixi <hi@huyixi.com>
2025-03-18 16:59:36 +0800

    chore

--- components/ToolCard.vue
+++ components/ToolCard.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="bg-white p-4 rounded-lg shadow-md">
-        <NuxtLink :to="tool.src">
+    <div class="bg-white p-4 rounded-lg shadow-md hover:pointer">
+        <NuxtLink :to="tool.url">
             <!-- <NuxtImg :src="tool.icon" alt="tool.name" class="h-12 w-12 mb-2" /> -->
             <h2 class="text-xl font-semibold">{{ tool.name }}</h2>
             <p class="text-gray-600">{{ tool.summary }}</p>
